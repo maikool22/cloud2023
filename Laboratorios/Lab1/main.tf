40fa453f86230b2d61adadf14e2fe1cd6055083f
William <william.sandoval@verifone.com>
2023-05-11 09:50:56 -0300

    modificando el practico 1

--- Laboratorios/Lab1/main.tf
+++ Laboratorios/Lab1/main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "name" {
-  
+resource "aws_instance" "Lab1-instance" {
+  ## las variables vacias las voy a parametrizar en el variables.tf
+  ami = ""
+  instance_type = "" ## 
+  key_name = ""
+  tags = {
+    Name =  ""
+  }
 }
