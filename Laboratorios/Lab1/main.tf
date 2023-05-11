43d9f4d73e834e863cabf3a577460f6b052aaf73
William <william.sandoval@verifone.com>
2023-05-11 10:02:25 -0300

    .

--- Laboratorios/Lab1/main.tf
+++ Laboratorios/Lab1/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "Lab1-instance" {
   ## las variables vacias las voy a parametrizar en el variables.tf
-  ami = ""
-  instance_type = "" ## 
-  key_name = ""
+  ## las llamo con el var.nombre de la variable
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
   tags = {
-    Name =  ""
+    Name =  var.Name
   }
 }
---
+++ Laboratorios/Lab1/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+    type = "string"
+    descdescription = "Variable para el id de la ami"  
+}
+
+variable "instance_type" {
+    type = "string"  
+}
+
+variable "key_name" {
+    type = "string"  
+}
+
+variable "Name" {
+    type = "string"  
+}
