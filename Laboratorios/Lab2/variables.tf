a6313e92ad503546fe8d7c46a458cdb18bf72b48
Damian Sandoval <will.ddr@gmail.com>
2023-05-12 21:11:44 -0300

    comienzo lab2

---
+++ Laboratorios/Lab2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  type        = string
+  description = "Variable para el id de la ami"
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
