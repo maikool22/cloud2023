416402682d0900f794333ff0bda6490c008ca898
Damian Sandoval <will.ddr@gmail.com>
2023-05-15 21:25:12 -0300

    kk

--- Laboratorios/Lab8/instances.tf
+++ Laboratorios/Lab8/instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webapp-server01" {
   instance_type          = "t2.large"
   key_name               = "vockey"
   vpc_security_group_ids = [aws_security_group.ssh-http-access.id]
-  subnet_id = [aws_subnet.subnet1.id]
+  subnet_id              = aws_subnet.subnet1-practico-3tier.id
 }
 
 resource "aws_instance" "webapp-server02" {
@@ -11,6 +11,5 @@ resource "aws_instance" "webapp-server02" {
   instance_type          = "t2.large"
   key_name               = "vockey"
   vpc_security_group_ids = [aws_security_group.ssh-http-access.id]
-    subnet_id = [aws_subnet.subnet2.id]
-
+  subnet_id              = aws_subnet.subnet2-practico-3tier.id
 }
--- Laboratorios/Lab8/network.tf
+++ Laboratorios/Lab8/network.tf
@@ -1,6 +1,6 @@
 #1 - Creamos la vpc
 resource "aws_vpc" "vpc-practico-3tier" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
